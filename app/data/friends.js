af9e50af6f25ac2dfb4d61dcbd745d4c3a6bbd2a
isai <Isai4827@4827@gmail.com>
2019-02-25 22:36:58 -0700

    ...

--- app/data/friends.js
+++ app/data/friends.js
@@ -13,7 +13,13 @@ app.listen(PORT, function(){
 
 
 var Friends = []
-for()
 
+// this for loop is suppose to be going through the array of the scores that each person gave and looking for any diference in the number the returning an alert that says which peron is your match 
+for (var i = 0; i < Friends.uniqueID.length; i++){
+    if (Friends.uniqueID === newFriend.uniqueID){
+        return alert(Friends.name)
+    }
+}
 
-// code to deploy to heroku which i couldn't get 
+// push the match of your score onto an alert that would pop up once it was done going through the array of people 
+app.push()
