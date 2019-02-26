3592f6b05c24e239a45ad300a7286b27c92e4c17
isai <Isai4827@4827@gmail.com>
2019-02-26 09:51:43 -0700

    ...

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,4 @@
-var express = require("express");
-var path = require("path");
-var app = express();
-var PORT = process.env.PORT || 3000;
+
 var htmlRouting = require("routing/htmlRoutes.js");
 var apiRouting = require("routing/apiRoutes.js");
 
@@ -17,9 +14,10 @@ var Friends = []
 // this for loop is suppose to be going through the array of the scores that each person gave and looking for any diference in the number the returning an alert that says which peron is your match 
 for (var i = 0; i < Friends.uniqueID.length; i++){
     if (Friends.uniqueID === newFriend.uniqueID){
+        // push the match of your score onto an alert that would pop up once it was done going through the array of people 
         return alert(Friends.name)
     }
 }
 
-// push the match of your score onto an alert that would pop up once it was done going through the array of people 
-app.push()
+
+module.export
---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var path = require("path");
+var app = express();
+var PORT = process.env.PORT || 3000;
+var htmlRouting = require("routing/htmlRoutes.js");
+var apiRouting = require("routing/apiRoutes.js");
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.listen(PORT, function(){
+console.log("we made it")
+})
+
+// this for loop is suppose to be going through the array of the scores that each person gave and looking for any diference in the number the returning an alert that says which peron is your match 
+for (var i = 0; i < Friends.uniqueID.length; i++){
+    if (Friends.uniqueID === newFriend.uniqueID){
+        return alert(Friends.name)
+    }
+}
+
+// push the match of your score onto an alert that would pop up once it was done going through the array of people 
+app.push()
