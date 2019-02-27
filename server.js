91100f9f4efaec7eae4781330c19300f371307e1
isai <Isai4827@4827@gmail.com>
2019-02-27 10:03:37 -0700

    ....

--- server.js
+++ server.js
@@ -14,13 +14,3 @@ app.use(apiRouting);
 app.listen(PORT, function(){
 console.log("we made it")
 })
-
-// this for loop is suppose to be going through the array of the scores that each person gave and looking for any diference in the number the returning an alert that says which peron is your match 
-for (var i = 0; i < friends.uniqueID.length; i++){
-    if (Friends.uniqueID === newFriend.uniqueID){
-        return alert(Friends.name)
-    }
-}
-
-// push the match of your score onto an alert that would pop up once it was done going through the array of people 
-app.push()
