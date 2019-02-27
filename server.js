7ab230daebc3a48dedefb5f142905ae459f3c190
isai <Isai4827@4827@gmail.com>
2019-02-27 10:02:00 -0700

    ...

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ console.log("we made it")
 })
 
 // this for loop is suppose to be going through the array of the scores that each person gave and looking for any diference in the number the returning an alert that says which peron is your match 
-for (var i = 0; i < Friends.uniqueID.length; i++){
+for (var i = 0; i < friends.uniqueID.length; i++){
     if (Friends.uniqueID === newFriend.uniqueID){
         return alert(Friends.name)
     }
